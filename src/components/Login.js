cfff5419065fc0dba34a602e36ae0f055e707d68
Yogeshwargundale <yogeshwargundale@gmail.com>
2019-08-27 16:12:03 +0530

    added login page in card and setting alignment

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,38 @@
 import React from 'react';
-import { Form,Input, Button } from 'antd';
+import { Form,Input, Button,Card, Row, Col } from 'antd';
 import "antd/dist/antd.css";
 import "../login.css";
 class Login extends React.Component{
     render(){
         return(
-            <div>
-                <h3>Login page</h3>
-                <Form layout="vertical">
-                <div class="input-custom">
-                    <Input placeholder="email_id"></Input><br />
-                    <Input placeholder="password"></Input><br />
-                </div>
+            <div style={{align:"center"}}>
+            <br /><br /><br />
+
+                <Row justify={"center"}>
+                    {/* <Col span={8}></Col> */}
+
+ {/* Login Card  */}
+
+                    <Col span={12} offset={6}>
+                    <Card title="Login Form" style={{width:300 ,height:300}}>
+
+<Form layout="vertical">
+<div class="input-custom">
+    <Input placeholder="email id"></Input><br />
+    <Input placeholder="password"></Input><br />
+</div>
+
+<Button type="primary">sign in</Button>
+</Form>
+
+</Card>
+                    </Col>
+
+
+                    {/* <Col span={8}></Col> */}
+                </Row>
                 
-                <Button type="primary">sign in</Button>
-                </Form>
+                
             </div>
         )
     }
