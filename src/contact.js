bcfbd055f192de504429c685bcda4a1468d2a8f7
Yogeshwargundale <yogeshwargundale@gmail.com>
2019-08-28 12:09:31 +0530

    added info

---
+++ src/contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Contact extends React.Component{
+
+    render(){
+
+        return(
+            <div>
+                Welcome to Contact
+            </div>
+        )
+    }
+}
+export default Contact;
